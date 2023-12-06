f10c89fa51cc2ee80c7555e7c92dd9ae65836572
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2023-12-06 16:39:26 +0100

    Add day-5/part-2 solution

---
+++ day-5/part-2/src/main.rs
@@ -0,0 +1,365 @@
+use rayon::prelude::*;
+use std::fs;
+
+fn get_file_content(file_path: &String) -> String {
+    println!("Loading input file: {}", file_path);
+    fs::read_to_string(file_path).expect("Cannot load file")
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Seed {
+    start: u32,
+    range: u32,
+}
+
+type SeedMap = Vec<Seed>;
+
+fn parse_seeds(line: &str) -> SeedMap {
+    let mut out: Vec<(u32, u32)> = vec![];
+    let mut tuple: (u32, u32) = (0, 0);
+    for (index, string) in line.replace("seeds: ", "").split(" ").enumerate() {
+        let parsed = string.parse().unwrap();
+        if index % 2 == 1 {
+            tuple.0 = parsed;
+            out.push(tuple);
+        } else {
+            tuple.1 = parsed;
+        }
+    }
+    return out
+        .iter()
+        .map(|t| Seed {
+            start: t.1,
+            range: t.0,
+        })
+        .collect();
+}
+
+#[cfg(test)]
+mod tests_parse_seeds {
+    use super::*;
+
+    #[test]
+    fn parse_seed_ok() {
+        assert_eq!(
+            parse_seeds("seeds: 1 3 11 42"),
+            vec![
+                Seed { start: 1, range: 3 },
+                Seed {
+                    start: 11,
+                    range: 42
+                }
+            ]
+        );
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Info {
+    destination_range_start: u32,
+    source_range_start: u32,
+    range_length: u32,
+}
+
+type Map = Vec<Info>;
+
+trait Sort {
+    fn sort(&mut self);
+}
+
+impl Sort for Map {
+    fn sort(&mut self) {
+        self.sort_by(|a, b| {
+            a.source_range_start
+                .partial_cmp(&b.source_range_start)
+                .unwrap()
+        });
+    }
+}
+
+fn parse_to_map(lines: &Vec<String>) -> Map {
+    let mut out = vec![];
+    for line in lines {
+        let mut split = line.split(' ');
+        out.push(Info {
+            destination_range_start: split.next().unwrap().parse().unwrap(),
+            source_range_start: split.next().unwrap().parse().unwrap(),
+            range_length: split.next().unwrap().parse().unwrap(),
+        });
+    }
+    out.sort();
+    return out;
+}
+
+#[cfg(test)]
+mod tests_parse_to_map {
+    use super::*;
+
+    #[test]
+    fn parse_to_map_01() {
+        assert_eq!(
+            parse_to_map(&vec![String::from("3 1 5"), String::from("6 3 2")]),
+            vec![
+                Info {
+                    destination_range_start: 3,
+                    source_range_start: 1,
+                    range_length: 5
+                },
+                Info {
+                    destination_range_start: 6,
+                    source_range_start: 3,
+                    range_length: 2
+                }
+            ]
+        );
+    }
+}
+
+/// map better be sorted ! Use .sort() for that
+fn get_destination(source: u32, map: &Map) -> u32 {
+    if map.len() == 0 {
+        return source;
+    }
+    let entry: &Info = {
+        match map.iter().filter(|x| source >= x.source_range_start).last() {
+            Some(mapping) => mapping,
+            _ => map.last().unwrap(),
+        }
+    };
+    let delta = (source as i64 - entry.source_range_start as i64) as u32;
+    if delta <= entry.range_length {
+        entry.destination_range_start + delta
+    } else {
+        source
+    }
+}
+
+#[cfg(test)]
+mod tests_get_destination {
+    use super::*;
+
+    #[test]
+    fn get_destination_found() {
+        assert_eq!(
+            get_destination(
+                3,
+                &vec![
+                    Info {
+                        destination_range_start: 1,
+                        source_range_start: 2,
+                        range_length: 5,
+                    },
+                    Info {
+                        destination_range_start: 10,
+                        source_range_start: 15,
+                        range_length: 9,
+                    },
+                ]
+            ),
+            2
+        );
+    }
+
+    #[test]
+    fn get_destination_not_found() {
+        assert_eq!(
+            get_destination(
+                999,
+                &vec![
+                    Info {
+                        destination_range_start: 1,
+                        source_range_start: 2,
+                        range_length: 5,
+                    },
+                    Info {
+                        destination_range_start: 10,
+                        source_range_start: 15,
+                        range_length: 9,
+                    },
+                ]
+            ),
+            999
+        );
+    }
+
+    #[test]
+    fn get_destination_out_of_range() {
+        assert_eq!(
+            get_destination(
+                22,
+                &vec![
+                    Info {
+                        destination_range_start: 1,
+                        source_range_start: 2,
+                        range_length: 5,
+                    },
+                    Info {
+                        destination_range_start: 10,
+                        source_range_start: 15,
+                        range_length: 3,
+                    },
+                    Info {
+                        destination_range_start: 10,
+                        source_range_start: 45,
+                        range_length: 100,
+                    },
+                ]
+            ),
+            22
+        );
+    }
+
+    #[test]
+    fn get_destination_last_entry() {
+        assert_eq!(
+            get_destination(
+                62,
+                &vec![
+                    Info {
+                        destination_range_start: 1,
+                        source_range_start: 2,
+                        range_length: 5,
+                    },
+                    Info {
+                        destination_range_start: 10,
+                        source_range_start: 15,
+                        range_length: 9,
+                    },
+                    Info {
+                        destination_range_start: 10,
+                        source_range_start: 45,
+                        range_length: 100,
+                    },
+                ]
+            ),
+            27
+        );
+    }
+}
+
+fn main() {
+    let content = get_file_content(&String::from("assets/input"));
+
+    let mut seeds: SeedMap = vec![];
+    let mut seed_to_soil_map: Map = vec![];
+    let mut soil_to_fertilizer_map: Map = vec![];
+    let mut fertilizer_to_water_map: Map = vec![];
+    let mut water_to_light_map: Map = vec![];
+    let mut light_to_temperature_map: Map = vec![];
+    let mut temperature_to_humidity_map: Map = vec![];
+    let mut humidity_to_location_map: Map = vec![];
+
+    let mut acc: Vec<String> = vec![];
+    let mut lines = content.lines();
+    while let Some(line) = lines.next() {
+        match line {
+            l if l.starts_with("seeds:") => {
+                seeds = parse_seeds(line);
+            }
+            l if l.starts_with("seed-to-soil") => {
+                while let Some(l) = lines.next() {
+                    if l == "" {
+                        break;
+                    }
+                    acc.push(String::from(l));
+                }
+                seed_to_soil_map = parse_to_map(&acc);
+                acc = vec![];
+            }
+            l if l.starts_with("soil-to-fertilizer") => {
+                while let Some(l) = lines.next() {
+                    if l == "" {
+                        break;
+                    }
+                    acc.push(String::from(l));
+                }
+                soil_to_fertilizer_map = parse_to_map(&acc);
+                acc = vec![];
+            }
+            l if l.starts_with("fertilizer-to-water") => {
+                while let Some(l) = lines.next() {
+                    if l == "" {
+                        break;
+                    }
+                    acc.push(String::from(l));
+                }
+                fertilizer_to_water_map = parse_to_map(&acc);
+                acc = vec![];
+            }
+            l if l.starts_with("water-to-light") => {
+                while let Some(l) = lines.next() {
+                    if l == "" {
+                        break;
+                    }
+                    acc.push(String::from(l));
+                }
+                water_to_light_map = parse_to_map(&acc);
+                acc = vec![];
+            }
+            l if l.starts_with("light-to-temperature") => {
+                while let Some(l) = lines.next() {
+                    if l == "" {
+                        break;
+                    }
+                    acc.push(String::from(l));
+                }
+                light_to_temperature_map = parse_to_map(&acc);
+                acc = vec![];
+            }
+            l if l.starts_with("temperature-to-humidity") => {
+                while let Some(l) = lines.next() {
+                    if l == "" {
+                        break;
+                    }
+                    acc.push(String::from(l));
+                }
+                temperature_to_humidity_map = parse_to_map(&acc);
+                acc = vec![];
+            }
+            l if l.starts_with("humidity-to-location") => {
+                while let Some(l) = lines.next() {
+                    if l == "" {
+                        break;
+                    }
+                    acc.push(String::from(l));
+                }
+                humidity_to_location_map = parse_to_map(&acc);
+                acc = vec![];
+            }
+            _ => {}
+        }
+    }
+
+    println!(
+        "Location: {}",
+        seeds
+            .par_iter()
+            .flat_map(|seed| {
+                (seed.start..seed.start + seed.range)
+                    .into_par_iter()
+                    .map(|i| {
+                        get_destination(
+                            get_destination(
+                                get_destination(
+                                    get_destination(
+                                        get_destination(
+                                            get_destination(
+                                                get_destination(i, &seed_to_soil_map),
+                                                &soil_to_fertilizer_map,
+                                            ),
+                                            &fertilizer_to_water_map,
+                                        ),
+                                        &water_to_light_map,
+                                    ),
+                                    &light_to_temperature_map,
+                                ),
+                                &temperature_to_humidity_map,
+                            ),
+                            &humidity_to_location_map,
+                        )
+                    })
+            })
+            .min()
+            .unwrap()
+    );
+}
